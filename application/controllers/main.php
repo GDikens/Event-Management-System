35cfd93fca84ee104110b208c066454780b37c99
Dineth Kariyawasam <dinethkariyawasam@yahoo.com>
2017-09-27 13:28:40 +0530

    created the structure

---
+++ application/controllers/main.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dineth Kariyawasam
+ * Date: 9/27/2017
+ * Time: 1:23 PM
+ */
+class Main extends CI_Controller{
+
+    function index(){
+
+        $this->load->view('index');
+
+    }
+}
