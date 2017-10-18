39d9fb0c90a43c4a1cbbd5f3634c74b31677d611
Dineth Kariyawasam <dinethkariyawasam@yahoo.com>
2017-10-18 13:23:54 +0530

    calendar built

--- application/views/index.php
+++ application/views/index.php
@@ -127,7 +127,7 @@
                         <ul class="dropdown-menu extended logout">
                             <div class="log-arrow-up"></div>
                             <li class="eborder-top">
-                                <a href="#"><i class="icon_profile"></i> My Profile</a>
+                                <a href="profile"><i class="icon_profile"></i> My Profile</a>
                             </li>
                             <li>
                                 <a href="#"><i class="icon_mail_alt"></i> My Inbox</a>
@@ -215,17 +215,389 @@
 				<div class="col-lg-12">
 					<h3 class="page-header"><i class="fa fa-laptop"></i> My Calender</h3>
 				</div>
-			</div>
+                </div>
+          </section>
               
             <div class="row">
+                <div class="col-sm-1">
 
+                </div>
+                <div class="col-sm-10">
+                    <table class="table table-bordered" id="mycalendar">
+                        <thead>
+                            <tr>
+                                <th>Mon</th>
+                                <th>Tue</th>
+                                <th>Wed</th>
+                                <th>Thu</th>
+                                <th>Fri</th>
+                                <th>Sat</th>
+                                <th>Sun</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto ">
+                                        <div class="fc-day-number">25</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">26</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">27</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">28</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">29</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">30</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">1</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto ">
+                                        <div class="fc-day-number">2</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">3</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">4</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">5</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">6</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">7</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">8</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto ">
+                                        <div class="fc-day-number">9</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">10</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">11</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">12</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">13</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">14</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">15</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto ">
+                                        <div class="fc-day-number">16</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">17</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">18</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">19</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">20</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">21</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">22</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">23</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">24</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">25</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">26</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">27</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">28</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">29</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">30</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">31</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">1</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">2</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">3</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">4</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="mycal-div" style="min-height: 106px;width: auto">
+                                        <div class="fc-day-number">5</div>
+                                        <div class="fc-day-content">
+
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-sm-1">
+
+                </div>
 				
 			</div><!--/.row-->
 
 
 
       <!--main content end-->
-            </section>
+
+        </section>
   <!-- container section start -->
 
     <!-- javascripts -->
